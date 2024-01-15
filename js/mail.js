39482952bd8f0549fb8a6611572140a0fcf7a427
poloztech <poloztech.ug@gmail.com>
2024-01-15 14:53:25 +0300

    other

--- js/mail.js
+++ js/mail.js
@@ -2,16 +2,29 @@ function sendEmail(){
     let email = document.getElementById('email').value;
 let first_name = document.getElementById('first-name').value;
 let last_name = document.getElementById('last-name').value;
-let message = document.getElementById('message').value;
 let form = document.getElementById('form');
 
 emailjs.send("service_brc1q5s","template_8wjruar",{
     email,
     first_name,
-    last_name,
-    message
+    last_name
 })
 form.reset();
 }
 
-function sendVolunteerEmail(){}
+function sendVolunteerEmail(){
+    let volunteer_name = document.getElementById('volunteer-name').value;
+    let volunteer_email = document.getElementById('volunteer-email').value;
+    let volunteer_subject = document.getElementById('volunteer-subject').value;
+    let volunteer_message = document.getElementById('volunteer-message').value;
+    let form = document.getElementById('form');
+
+    emailjs.send("service_brc1q5s","template_c6732fh",{
+        volunteer_name,
+        volunteer_subject,
+        volunteer_message,
+        volunteer_email,
+        email,
+})
+form.reset();
+}
