52dd08c388cd46f3fa0acdea7907e1db7fb43b43
techy <pauldrums.ug@gmail.com>
2024-03-23 15:30:48 +0300

    new

---
+++ public/js/custom.js
@@ -0,0 +1,42 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // COUNTER NUMBERS
+    jQuery('.counter-thumb').appear(function() {
+      jQuery('.counter-number').countTo();
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+});
+    
+  })(window.jQuery);
+
+  
+
+  function openLightbox(imageSrc, caption) {
+    document.getElementById('lightboxImage').src= imageSrc;
+    document.querySelector('.lightbox').classList.add('active');
+    document.querySelector('.lightbox img').alt= caption;
+}
+function closeLightbox() {
+    document.querySelector('.lightbox').classList.remove('active');
+}
